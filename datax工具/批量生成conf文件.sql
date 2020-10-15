228ee8ba3605373d62cee579e0fb5076ceae1cd0
thinkpad <atyongsi@163.com>
2020-10-15 11:42:17 +0800

    datax tools

---
+++ datax工具/批量生成conf文件.sql
@@ -0,0 +1,68 @@
+SELECT
+    TABLE_NAME,
+'
+ {
+  "job": {
+    "setting": {
+        "speed": {
+          "byte":1048576,
+          "channel": 3
+        },
+        "errorLimit": {
+           "record": 0,
+           "percentage": 0.02
+        }
+    },
+    "content": [{
+      "reader": {
+          "name": "oraclereader",
+          "parameter": {
+            "username": "GHZGG45",
+            "password": "GHZGG45",
+            "connection": [{
+              "querySql": [
+                "SELECT ' || rtrim(xmlagg(xmlparse(content a.COL_NAME_TEMP||',' wellformed) ORDER BY COLUMN_ID).getclobval(),',') ||' FROM ' || a.TABLE_NAME || '"
+              ],
+              "jdbcUrl": [
+                "jdbc:oracle:thin:@192.168.100.133:1521:orcl"
+              ]
+            }]
+          }
+        },
+      "writer": {
+          "name": "oraclewriter",
+          "parameter": {
+              "column": [
+                  "'|| rtrim(xmlagg(xmlparse(content a.COLUMN_NAME||'","' wellformed) ORDER BY COLUMN_ID).getclobval(),'","') ||'"
+              ],
+              "password":"${ghdw_pswd}",
+              "username":"${ghdw_user}",
+               "connection":[{
+                      "jdbcUrl":"${ghdw_url}",
+                      "table":[
+                          "ODS_TS32_'||a.TABLE_NAME||'"
+                      ]
+                }]
+            }
+
+        }
+    }]
+  }
+}
+'
+FROM
+    (
+SELECT
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_ID,
+CASE
+    WHEN DATA_TYPE = 'DATE' THEN
+    'TO_CHAR(' || COLUMN_NAME || ',' || '''YYYYMMDD''' || ')' ELSE COLUMN_NAME 
+    END AS COL_NAME_TEMP 
+FROM
+    USER_TAB_COLS 
+WHERE
+    TABLE_NAME IN ( 'TSTOCKBUSINESS','TENTRUSTS' )) a 
+GROUP BY
+    TABLE_NAME
