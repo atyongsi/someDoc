cdb0d9f90eaebcc9ef693e508529f62113b81143
thinkpad <atyongsi@163.com>
2021-01-06 16:02:06 +0800

    更改

--- database/oracle/常见问题.sql
+++ database/oracle/常见问题.sql
@@ -11,17 +11,19 @@ select object_name,s.sid,s.serial#
 
 alter system kill session 'sid,serial#';
 
---3.如果数据库级不能杀掉该进程，需要到操作系统级别来处理
+--3.如果报错ORA-00031：标记要终止的会话
+--数据库级不能杀掉该进程，需要到操作系统级别来处理
 select a.spid,b.sid,b.serial#,b.username 
 from v$process a,v$session b 
 where a.addr=b.paddr and b.status='KILLED';
 
 --获取进程号 spid
+select * from dba_ddl_locks where owner = 'GHDW';  查询sid
 select b.spid,a.osuser,b.program from v$session a,v$process b
 where a.paddr=b.addr and a.sid=33    --33就是上面的sid
 
 --在OS上杀死进程
---1）在unix上，用root身份执行命令：#kill -9 spid
+--1）在unix上，用root身份执行命令：kill -9 spid
 --2）在windows（unix也适用）用orakill杀死线程，orakill是oracle提供的一个可执行命令，语法为：orakill sid thread 
 -- sid：表示要杀死的进程属于的实例名   thread：是要杀掉的线程号，即第3步查询出的spid。
 
