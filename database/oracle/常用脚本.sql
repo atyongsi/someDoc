1a30459061e41a511edb3d5a0a5f8a578c8f5fa8
thinkpad <atyongsi@163.com>
2021-01-06 14:10:09 +0800

    更新

--- database/oracle/常用脚本.sql
+++ database/oracle/常用脚本.sql
@@ -1,3 +1,37 @@
+--重启oracle实例
+（1） 切换需要启动的数据库实例：export ORACLE_SID=C1
+（2） 进入Sqlplus控制台，命令：sqlplus /nolog
+（3） 以系统管理员登录，命令：connect / as sysdba
+（4） 如果是关闭数据库，命令：shutdown abort
+（5） 启动数据库，命令：startup
+（6） 退出sqlplus控制台，命令：exit
+
+--重启oracle服务和监听
+方法1：
+用root以ssh登录到linux，打开终端输入以下命令：
+cd $ORACLE_HOME #进入到oracle的安装目录 
+dbstart #重启服务器 
+lsnrctl start #重启监听器 
+cd $ORACLE_HOME #进入到oracle的安装目录
+dbstart #重启服务器
+lsnrctl start #重启监听器
+
+方法2：
+cd $ORACLE_HOME/bin #进入到oracle的安装目录 
+./dbstart #重启服务器 
+./lsnrctl start #重启监听器
+
+方法3：
+（1） 以oracle身份登录数据库，命令：su -oracle
+（2） 进入Sqlplus控制台，命令：sqlplus /nolog
+（3） 以系统管理员登录，命令：connect / as sysdba
+（4） 启动数据库，命令：startup
+（5） 如果是关闭数据库，命令：shutdown immediate
+（6） 退出sqlplus控制台，命令：exit
+（7） 进入监听器控制台，命令：lsnrctl
+（8） 启动监听器，命令：start
+（9） 退出监听器控制台，命令：exit
+
 
 --批量清空某一类型的表
 DECLARE
@@ -9,5 +43,14 @@ BEGIN
   END LOOP;
 END;
 
-
-
+--数据泵导入导出数据
+1、测试环境数据泵导出命令：
+1）、创建DIRECTORY（新环境需要，若之前已经建过文件夹后，此次就不需要再建了，直接使用之前建的即可）
+createorreplacedirectory DMP_GHDW as'/opt/DMP_GHDW';
+2）、查看DIRECTORY
+select * from dba_directories;
+3）、为超级管理员赋予操作逻辑目录操作权限（一般不需要设置此步骤，但有时是必须。我们使用超级管理员用户建的，超级管理员已经有该文件夹的操作权限了，所以不需要再赋权了。）
+grant read,write on directory DMP_GHDW to sys;
+注：以上命令在plsql中执行，在linux环境下执行无效。
+4）、执行导出命令（directory=DMPDIR，文件夹使用之前别人建过的，确认确实存在该文件夹即可。）
+expdp GHDW/GHDW@172.16.11.17/PDBTEST2  directory=DMPDIR dumpfile=GHDW20201216.dmp logfile=GHDW20201216.log
