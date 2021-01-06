2dfa1889e2c667942305b3b117a16218d963a2c2
thinkpad <atyongsi@163.com>
2021-01-06 15:17:15 +0800

    ...

--- database/oracle/常用脚本.sql
+++ database/oracle/常用脚本.sql
@@ -83,6 +83,82 @@ GROUP BY
 	a.TABLE_NAME
 ;
 
+-- 常用系统表：USER_TAB_COLS,USER_TABLES,USER_TAB_COMMENTS,USER_COL_COMMENTS
+
+--创建表空间，指定数据存放位置
+CREATE tablespace MY_TABLESPACE datafile 'C:\software\oracle_data_store\MY_TABLESPACE.dbf' size 100M autoextend on next 10M maxsize unlimited;
+--创建用户
+CREATE USER wys IDENTIFIED BY admin1000 --用户密码
+DEFAULT tablespace MY_TABLESPACE -- 表空间是上面创建的
+temporary tablespace TEMP -- 临时表空间默认 TEMP;
+--用户授权，普通用户可以不加dba
+grant connect,resource,dba to wys;
+
+--查看表空间使用情况
+SELECT a.tablespace_name "表空间名称", 
+total / (1024 * 1024) "表空间大小(M)", 
+free / (1024 * 1024) "表空间剩余大小(M)", 
+(total - free) / (1024 * 1024 ) "表空间使用大小(M)", 
+total / (1024 * 1024 * 1024) "表空间大小(G)", 
+free / (1024 * 1024 * 1024) "表空间剩余大小(G)", 
+(total - free) / (1024 * 1024 * 1024) "表空间使用大小(G)", 
+round((total - free) / total, 4) * 100 "使用率 %" 
+FROM (SELECT tablespace_name, SUM(bytes) free 
+FROM dba_free_space 
+GROUP BY tablespace_name) a, 
+(SELECT tablespace_name, SUM(bytes) total 
+FROM dba_data_files 
+GROUP BY tablespace_name) b 
+WHERE a.tablespace_name = b.tablespace_name
+
+SELECT
+	file_name 物理文件名,
+	tablespace_name 表空间,
+	bytes / 1024 / 1024 / 1024 大小G 
+FROM
+	dba_data_files WHERE tablespace_name = 'GHDW' 
+GROUP BY
+	file_name,tablespace_name,bytes
+
+--查看表空间是否自动增长
+SELECT FILE_NAME "文件路径",
+TABLESPACE_NAME "表空间名字",
+AUTOEXTENSIBLE "是否自动增长" FROM dba_data_files;
+
+--设置表空间自动增长
+ALTER DATABASE DATAFILE '/表空间路径/表空间文件名称.dbf' AUTOEXTEND ON;//打开自动增长
+ALTER DATABASE DATAFILE '/表空间路径/表空间文件名称.dbf' AUTOEXTEND ON NEXT 200M ;//每次自动增长200M
+ALTER DATABASE DATAFILE '/表空间路径/表空间文件名称.dbf' AUTOEXTEND ON NEXT 200M MAXSIZE 1024M;//每次自动增长200M，表空间最大不超过1G
+
+ORA-01652：临时表空间不足
+--所有表空间查看
+select * from dba_tablespaces;
+--临时表空间查看
+select * from dba_temp_files;
+--临时表空间自动增长
+alter database tempfile '/oradata/efss/temp01.dbf' autoextend on next 100m;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ database/oracle/常见问题.sql
@@ -0,0 +1,53 @@
+--ORA-04021，等待锁定对象时发生超时
+--1.定位问题，找到哪条sql引起的锁表
+select * from dba_ddl_locks where owner = 'GHDW';
+select * from v$session where SID = 637
+select * from v$sql where sql_id='41ax27r0cz1nt'
+
+--2.解决问题，如果表上发生了死锁，kill掉进程
+select object_name,s.sid,s.serial# 
+  from v$locked_object l,dba_objects o ,v$session s
+ where l.object_id　=　o.object_id and l.session_id=s.sid;
+
+alter system kill session 'sid,serial#';
+
+--3.如果数据库级不能杀掉该进程，需要到操作系统级别来处理
+select a.spid,b.sid,b.serial#,b.username 
+from v$process a,v$session b 
+where a.addr=b.paddr and b.status='KILLED';
+
+--获取进程号 spid
+select b.spid,a.osuser,b.program from v$session a,v$process b
+where a.paddr=b.addr and a.sid=33    --33就是上面的sid
+
+--在OS上杀死进程
+--1）在unix上，用root身份执行命令：#kill -9 spid
+--2）在windows（unix也适用）用orakill杀死线程，orakill是oracle提供的一个可执行命令，语法为：orakill sid thread 
+-- sid：表示要杀死的进程属于的实例名   thread：是要杀掉的线程号，即第3步查询出的spid。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
